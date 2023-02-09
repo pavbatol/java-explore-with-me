1a09a2e1a5e26a5d6a968a62da5e3d0ca6b9e194
P.Sergey <pavbatol@yandex.ru>
2023-02-09 21:04:11 +0400

    fix:main: Correct UserMapper, schema

--- ewm-main/src/main/java/ru/practicum/ewm/user/model/UserMapper.java
+++ ewm-main/src/main/java/ru/practicum/ewm/user/model/UserMapper.java
@@ -1,8 +1,12 @@
 package ru.practicum.ewm.user.model;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.ewm.app.BaseMapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper extends BaseMapper<User, UserDto> {
+    @Override
+    @Mapping(target = "observable", source = "observable", defaultValue = "true")
+    User toEntity(UserDto dto);
 }
