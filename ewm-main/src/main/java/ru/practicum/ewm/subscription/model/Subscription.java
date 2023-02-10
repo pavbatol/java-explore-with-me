b4c93ed445ded41132b6d15ee9492fd7b144499e
P.Sergey <pavbatol@yandex.ru>
2023-02-11 00:07:58 +0400

    fix:main: Code style

--- ewm-main/src/main/java/ru/practicum/ewm/subscription/model/Subscription.java
+++ ewm-main/src/main/java/ru/practicum/ewm/subscription/model/Subscription.java
@@ -29,39 +29,10 @@ public class Subscription {
     @JoinColumn(name = "owner_id", nullable = false)
     User owner;
 
-    @ManyToMany
+    @OneToMany
     @JoinTable(name = "subscriptions_favorites",
             joinColumns = @JoinColumn(name = "sbr_id", nullable = false),
             inverseJoinColumns = @JoinColumn(name = "fav_id", nullable = false))
     @ToString.Exclude
     Set<User> favorites;
 }
-
-/*
-@Setter
-@Getter
-@ToString
-@Accessors(chain = true)
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@Entity
-@Table(name = "subscriptions")
-public class Subscription {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sbr_id", nullable = false)
-    Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "subscriber_id", nullable = false)
-    User subscriber;
-
-    @ManyToOne
-    @JoinColumn(name = "favorite_id", nullable = false)
-    User favorite;
-
-    @Enumerated(EnumType.STRING)
-    SubscriptionState state;
-}
-
- */
